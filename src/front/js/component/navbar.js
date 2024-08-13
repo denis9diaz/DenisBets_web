6e5aa6f01e45f995dec1d6baf5a290c559e1011a
Denis Díaz <150247992+denis9diaz@users.noreply.github.com>
2024-08-13 09:59:56 +0000

    version inicial

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,17 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import Logo from "/src/front/img/logo.png"
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
+		<nav className="navbar navbar-expand-lg bg-body-tertiary">
+			<div className="container-fluid">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<img src={Logo} alt="logo" className="imageLogo" />
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon">
+						<FontAwesomeIcon icon={faBars} />
+					</span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<a className="nav-link active" aria-current="page" href="#">Home</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">Link</a>
+						</li>
+						<li className="nav-item dropdown">
+							<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								Dropdown
+							</a>
+							<ul className="dropdown-menu">
+								<li><a className="dropdown-item" href="#">Action</a></li>
+								<li><a className="dropdown-item" href="#">Another action</a></li>
+								<li><hr className="dropdown-divider" /></li>
+								<li><a className="dropdown-item" href="#">Something else here</a></li>
+							</ul>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link disabled" aria-disabled="true">Disabled</a>
+						</li>
+					</ul>
+					<form className="d-flex" role="search">
+						<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+						<button className="btn btn-outline-success" type="submit">Search</button>
+					</form>
 				</div>
 			</div>
 		</nav>
---
+++ src/front/js/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../styles/home.css";
+
+const Home = () => {
+	return (
+		<div className="text-center mt-5">
+			<h1>DenisBets</h1>
+		</div>
+	);
+};
+
+export default Home
