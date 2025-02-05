20c0bd326af3e63cd3a338216ee6edaaab84c6fd
Denis Díaz <150247992+denis9diaz@users.noreply.github.com>
2025-02-05 09:48:11 +0000

    modificado private y css

--- src/front/js/pages/PrivateSection.js
+++ src/front/js/pages/PrivateSection.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
-import WorldFlag from "react-world-flags";  // Librería para mostrar banderas
+import React from "react";
+import WorldFlag from "react-world-flags";
 import "../../styles/privatesection.css";
+import cuota from "/src/front/img/cuota.png";
+import stake from "/src/front/img/stake.png";
+import pick from "/src/front/img/pick.png";
 
 const PrivateSection = () => {
-    // Estado para manejar los pronósticos
-    const [pronosticos, setPronosticos] = useState([
+    const pronosticos = [
         {
             id: 1,
             fecha: "04/02/2025",
@@ -16,151 +18,41 @@ const PrivateSection = () => {
             tipo: "PRE",
             pais: "ES"  // Código de país (Ej. ES = España)
         }
-    ]);
-
-    // Estado para el formulario
-    const [nuevoPronostico, setNuevoPronostico] = useState({
-        fecha: "",
-        partido: "",
-        pronostico: "",
-        cuota: "",
-        stake: "",
-        notas: "",
-        tipo: "PRE",
-        pais: "ES" // Valor predeterminado
-    });
-
-    // Función para manejar los cambios en el formulario
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setNuevoPronostico({
-            ...nuevoPronostico,
-            [name]: value,
-        });
-    };
-
-    // Función para agregar un nuevo pronóstico
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const nuevoPronosticoConId = {
-            ...nuevoPronostico,
-            id: pronosticos.length + 1, // Generamos un id único
-        };
-        setPronosticos([...pronosticos, nuevoPronosticoConId]);
-        setNuevoPronostico({
-            fecha: "",
-            partido: "",
-            pronostico: "",
-            cuota: "",
-            stake: "",
-            notas: "",
-            tipo: "PRE",
-            pais: "ES",
-        }); // Limpiar el formulario
-    };
+    ];
 
     return (
         <div className="private-section">
             <h2 className="private-section-title">Pronósticos Premium</h2>
-
             {/* Mostrar los pronósticos */}
             {pronosticos.map((pronostico) => (
                 <div key={pronostico.id} className="pronostico-card">
                     <p className="fecha">📅 {pronostico.fecha}</p>
                     <div className="partido-pronostico">
                         <h3 className="partido">
-                            {/* Reemplazar el icono 🏴 por la bandera del país */}
                             <WorldFlag code={pronostico.pais} style={{ width: 30, height: 20, marginRight: 10 }} />
                             {pronostico.partido}
                         </h3>
-                        <h3 className="pronostico">⚽ {pronostico.pronostico}</h3>
-                    </div>
-                    <p className="cuota">💲 @<strong>{parseFloat(pronostico.cuota).toFixed(2)}</strong></p>
-                    <p className="stake">📊 Stake <strong>{pronostico.stake}</strong></p>
-                    <p className="notas">📝 {pronostico.notas}</p>
-                    <div className={`badge ${pronostico.tipo === "LIVE" ? "live" : "pre"}`}>
-                        {pronostico.tipo === "LIVE" ? "🔴 LIVE" : "📌 PRE-PARTIDO"}
+                        <h3 className="pronostico">
+                            <span className="icono-pronostico"><img src={pick} className="pick-icon"></img></span>
+                            {pronostico.pronostico}
+                        </h3>
+
+                        <p className="cuota">
+                            <span className="icono-cuota"><img src={cuota} className="cuota-icon"></img></span>
+                            @<strong>{parseFloat(pronostico.cuota).toFixed(2)}</strong>
+                        </p>
+
+                        <p className="stake">
+                            <span className="icono-stake"><img src={stake} className="stake-icon"></img></span>
+                            Stake <strong>{pronostico.stake}</strong>
+                        </p>
+                        <p className="notas">📝 {pronostico.notas}</p>
+                        <div className={`badge ${pronostico.tipo === "LIVE" ? "live" : "pre"}`}>
+                            {pronostico.tipo === "LIVE" ? "🔴 LIVE" : "📌 PRE-PARTIDO"}
+                        </div>
                     </div>
                 </div>
             ))}
-
-            {/* Formulario para agregar nuevos pronósticos */}
-            <h3>Agregar nuevo pronóstico</h3>
-            <form onSubmit={handleSubmit}>
-                <label>Fecha:</label>
-                <input
-                    type="text"
-                    name="fecha"
-                    value={nuevoPronostico.fecha}
-                    onChange={handleChange}
-                    required
-                />
-                <label>Partido:</label>
-                <input
-                    type="text"
-                    name="partido"
-                    value={nuevoPronostico.partido}
-                    onChange={handleChange}
-                    required
-                />
-                <label>Pronóstico:</label>
-                <input
-                    type="text"
-                    name="pronostico"
-                    value={nuevoPronostico.pronostico}
-                    onChange={handleChange}
-                    required
-                />
-                <label>Cuota:</label>
-                <input
-                    type="text"
-                    name="cuota"
-                    value={nuevoPronostico.cuota}
-                    onChange={handleChange}
-                    required
-                />
-                <label>Stake:</label>
-                <input
-                    type="text"
-                    name="stake"
-                    value={nuevoPronostico.stake}
-                    onChange={handleChange}
-                    required
-                />
-                <label>Notas:</label>
-                <input
-                    type="text"
-                    name="notas"
-                    value={nuevoPronostico.notas}
-                    onChange={handleChange}
-                />
-                <label>Tipo:</label>
-                <select
-                    name="tipo"
-                    value={nuevoPronostico.tipo}
-                    onChange={handleChange}
-                    required
-                >
-                    <option value="PRE">PRE-PARTIDO</option>
-                    <option value="LIVE">LIVE</option>
-                </select>
-
-                <label>Selecciona el País:</label>
-                <select
-                    name="pais"
-                    value={nuevoPronostico.pais}
-                    onChange={handleChange}
-                    required
-                >
-                    <option value="ES">España</option>
-                    <option value="GB">Reino Unido</option>
-                    <option value="FR">Francia</option>
-                    <option value="DE">Alemania</option>
-                    {/* Puedes agregar más países aquí */}
-                </select>
-
-                <button type="submit">Agregar Pronóstico</button>
-            </form>
         </div>
     );
 };
